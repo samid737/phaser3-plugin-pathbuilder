a194a68ddbce70873025520dabbf9a380c5f76c6
Samid\Sami <dara_s@live.nl>
2018-04-02 22:04:46 +0200

    add pause, resume button

--- src/Scene.js
+++ src/Scene.js
@@ -55,6 +55,10 @@ Scene.prototype = {
         
         this.importbutton = this.middle.add.text(this.W -100, this.H - 200, 'import', null, null, null, this.import, [], this);
         this.exportbutton = this.middle.add.text(this.W -100, this.H - 100, 'export', null, null, null, this.export, [], this);
+
+        this.pausebutton = this.middle.add.text(10, this.H - 200,'pause',null,null,null, this.freeze,[], this);  
+        this.resumebutton = this.middle.add.text(10, this.H - 150,'resume',null,null,null, this.unfreeze,[], this);  
+        
         this.previewbutton = this.middle.add.text(10, this.H - 100, 'preview', null, null, null, this.preview, [], this);
 
         this.modelabel = this.middle.add.label(100, 20, 'mode: ', null, null, null, null, this);
