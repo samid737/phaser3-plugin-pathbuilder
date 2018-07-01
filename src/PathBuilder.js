e83c76b32a964c1b7b70a1976b9a74cb1f89f56b
Samid\Sami <dara_s@live.nl>
2018-07-01 18:06:39 +0200

    clean up PathBuilder.js

--- src/PathBuilder.js
+++ src/PathBuilder.js
@@ -8,35 +8,9 @@ export class PathBuilder extends Phaser.Plugins.BasePlugin
     {
         super(pluginManager);
         console.log(this);
-        game.scene.add('UI',Scene, true);
+        this.game.scene.add('UI',Scene, true);
 
     }
-
-    boot()
-    {
-        //var eventEmitter = this.systems.events;
-
-        //eventEmitter.on('shutdown', this.shutdown, this);
-        //eventEmitter.on('destroy', this.destroy, this);
-        //console.log(this.systems);
-        //
-        //TODO: rewrite according API
-       // this.systems.scenePlugin.add('UI', Scene, true);
-
-    }
-
-    // //  Called when a Scene shuts down, it may then come back again later (which will invoke the 'start' event) but should be considered dormant.
-    // shutdown()
-    // {
-    // }
-
-    //  Called when a Scene is destroyed by the Scene Manager. There is no coming back from a destroyed Scene, so clear up all resources here.
-    destroy()
-    {
-        this.shutdown();
-
-        this.scene = undefined;
-    }
-
+    
 }
 
